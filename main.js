04d9146f2529abb4b89fd086d988373981ace9d6
AnjaP <treyjapan@pm.me>
2023-05-26 15:47:38 +0200

    feat:add slider section

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,11 @@ export default {
 					'2xl': '6rem',
 				},
 			},
+			dropShadow: {
+				'text-sm': '1px 1px 0px rgba(0, 0, 0, 0.90)',
+				'text-md': '1px 2px 0px rgba(0, 0, 0, 0.90)',
+				'text-lg': '1px 4px 0px rgba(0, 0, 0, 0.90)',
+			},
 			fontFamily: {
 				sans: ['Outfit', 'sans-serif'],
 			},
